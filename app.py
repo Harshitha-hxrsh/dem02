0b851034530a1bf27616f11812fe2db302afaa37
Harshitha-hxrsh <harshitha201016@gmail.com>
2026-04-13 08:50:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({
+        "message": "Hello World!",
+        "status": "success",
+        "status_code": 200
+    })
+
+@app.route('/welcome')
+def welcome():
+    return jsonify({
+        "message": "Welcome to the Flask App 🚀",
+        "status": "success",
+        "status_code": 200
+    })
+
+@app.route('/about')
+def about():
+    return jsonify({
+        "message": "This is a sample Flask API",
+        "status": "success",
+        "status_code": 200
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
